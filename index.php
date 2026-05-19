96382b19f51cfcf168fd44af4c89d6082640fa35
keano802 <9028451@student.zadkine.nl>
2026-05-19 10:41:55 +0200

    verbeterde uitleg

--- index.php
+++ index.php
@@ -11,18 +11,14 @@
 <body>
 
   <h1>Welkom</h1>
-  <p>Je bent een 12 jarige jongen en bent opgesloten in de kelder van
-    je stiefmoeders huis. je moet de reservesleutel vinden om de kelder deur te openen 
-    als dat is gelukt kom je uit in de hal en dan moet je de sleutel van de voor deur vinden om 
-    uit het huis te ontsnappen.</p>
+  <p>Je bent een 12-jarige jongen en zit opgesloten in de kelder van je stiefmoeders huis.
+Om te ontsnappen, moet je eerst de reservesleutel vinden waarmee je de kelderdeur kunt openen.
+Als je eenmaal boven bent, kom je in de hal van het huis. Daar ligt ergens de sleutel van de voordeur verstopt.
+Lukt het jou om alle hints te vinden en op tijd uit het huis te ontsnappen?</p>
 
-  <p>De button hieronder is alleen voor testdoeleinden bij het opstarten van het project. Deze pagina is bedoeld voor de
-    uitleg van jullie escape room.</p>
-  <p>Via deze pagina gaat een team naar de aanmeldpagina</p>
+  <p> Je hebt 15 minuten de tijd om te ontsnappen, dus wees snel en slim! </p>
 
-
-  <button><a href="./rooms/room_1.php">Klik hier voor een demonstratie van kamer
-      1</a></button>
+  <button><a href="./rooms/room_1.php">Klik hier om de escape te starten </a></button>
 
 </body>
 
