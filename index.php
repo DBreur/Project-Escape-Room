d62612b557658968eb9622a009eb9b8a24efb61e
keano802 <9028451@student.zadkine.nl>
2026-06-01 08:00:55 +0200

    teams update

--- index.php
+++ index.php
@@ -13,6 +13,28 @@
   session_start();
   require 'dbcon.php';
 
+  function ensureTeamTables(PDO $db_connection) {
+      $db_connection->exec(
+          "CREATE TABLE IF NOT EXISTS teams (
+              id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+              team VARCHAR(255) NOT NULL UNIQUE
+          ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;"
+      );
+
+      $db_connection->exec(
+          "CREATE TABLE IF NOT EXISTS team_members (
+              id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+              team_id INT NOT NULL,
+              user_id INT NOT NULL,
+              UNIQUE KEY idx_team_member (team_id, user_id),
+              KEY idx_team_members_team (team_id),
+              KEY idx_team_members_user (user_id)
+          ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;"
+      );
+  }
+
+  ensureTeamTables($db_connection);
+
   $teamName = null;
   if (!empty($_SESSION['user_id'])) {
       $teamStmt = $db_connection->prepare('SELECT t.team FROM teams t JOIN team_members tm ON t.id = tm.team_id WHERE tm.user_id = ? LIMIT 1');
